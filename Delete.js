a051b84e3e029c3c842df549b11a591342ef20c3
boundary-cond <52094597+boundary-cond@users.noreply.github.com>
2019-08-20 13:26:18 +0900

    Add files via upload

---
+++ Delete.js
@@ -0,0 +1,26 @@
+﻿
+user ={dataset:[1,2], selected_id:3};
+module.exports = (robot) => {
+   
+    //消去//
+    robot.respond(/PNG$/i, (res) => {
+        res.send({
+            question: "本当に完済ですか？"
+        });
+        if(res.json.question === "本当に完済ですか？"){
+            robot.respond('yesno', (res1) => {   
+                if (res1.json.response === null) {
+                    res1.send("無効な値です");
+                } 
+                else if(res1.json.response === true){//データ消去 & 最初に戻る//
+                    res1.send("good");
+                    user.splice(selected_id);
+                    //data.state = LIST;
+                }
+                else if(res1.json.response === false){//選択画面に戻る//
+                    //data.state = SELECT;
+                }    
+            }); 
+        }
+     });            
+};
---
+++ Pay.js
@@ -0,0 +1,47 @@
+﻿data = {
+	state: "STATE",
+	item: {
+		"相手": "A",
+                "残高": 10000,
+                "一回当たりの額": 1000
+	}
+};
+user = {
+	dataset: [
+                 data
+	],
+	selected_id: 0
+}
+
+
+pay = 0;
+module.exports = (robot) => {
+    robot.respond(/ONG$/i, (res) => {
+                  res.send("支払い総額を〇〇円の形で入力してください");
+                  robot.respond(/([1-9]\d*|0)円$/, (res) => {
+                      res.send({
+                          question: "より"//data.item["相手"] + "さんの残高は\n"+ data.item["残高"] + "から" + (data.item["残高"] - res.match[1]) +"\nとなります。よろしいでしょうか？"
+                      });
+                      pay = res.match[1];     
+                       if(res.json.question === "より"){//data.item["相手"] + "さんの残高は\n"+ data.item["残高"] + "から" + (data.item["残高"] - pay) +"\nとなります。よろしいでしょうか？"){
+                          robot.respond('yesno', (res) => {
+                              if (res.json.response === null) {
+                                  res.send("無効な値です");
+                              } 
+                              else if(res.json.response == true){//支払い//
+                                  data.item["残高"] = (data.item["残高"]-pay);
+                                  res.send(data.item["相手"] + "さんの残高は\n"+ (data.item["残高"]) + "となりました。");
+                                  data.item ["一回当たりの額"] = ((2*data.item["一回当たりの額"]) -(pay));   
+                                  //data.state = LIST;
+                              }
+                              else if(res.json.response == false){//選択画面に戻る//
+                                  //data.state = SELECT;
+                              }
+                          });
+                  }                   
+                  });      
+                
+                   
+                  
+     });
+};
