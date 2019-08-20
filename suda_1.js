9db41c189ba1cb1b81b0aa7837ca9c59994084e8
b7p41068 <b7p41068@shonan.bunkyo.ac.jp>
2019-08-20 10:38:10 +0900

    メンバ変数の初期化？してみました

--- suda_1.js
+++ suda_1.js
@@ -7,34 +7,29 @@
 //   time - Reply with current time
 'use strict';
 
-var balance_yen = 20000;
 
-module.exports = (robot) => {
-  robot.respond(/情報/i, (res) => {
+let State = {
+  ID:{},
+  TARGET: {},
+  TOTAL: {},
+  DESCRIPTION: {},
+  DATE: {},
+  LIMIT: {},
+  DIVISION: {},
+  REM: {},
+  SPLIT:{},
+  FREQ:{},
+  LIST:{},
+  BORROW:{},
+  LEND:{},
+  SELECT:{},
+  DELETE:{},
+  EDIT:{},
+  PAY:{}
+};
 
 class data{
-  constructor( id, name,total,desc,date,deadline,balance,split,period) {
-    this.id = id;
-    this.name = name;
-    this.total = total;
-    this.desc = desc;
-    this.date = date;
-    this.deadline = deadline;
-    this.balance = balance;
-    this.split = split;
-    this.period = period;
+  constructor() {
+    this.State = State.LIST;
   }
-
-  getName() {
-    return this.name;
-}
-
-}
-
-var user1 = new data(1,"はる",20000,"お茶会",20190908,201909030,balance_yen,2,1);
-
-test = console.log(user1.getName());
-
-  res.send(test);
-});
 };
