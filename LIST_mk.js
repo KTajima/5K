f6e98071f089071d001b6c5dc053a506513668d6
monokurokiss <monokurokiss.yoi@icloud.com>
2019-08-20 13:04:12 +0900

    一覧追加

--- LIST_mk.js
+++ LIST_mk.js
@@ -1,7 +1,7 @@
 module.exports = (robot) => {
     
     
-    robot.respond(/リスト/i, (res) => {
+    robot.respond(/リスト$/i, (res) => {
 	
 	res.send('相手: ', Data.item["相手"], '総額: ', Data.item["総額"],
 		 '詳細: ', Data.item["詳細"], '貸借日: ', Data.item["賃借日"],
@@ -11,34 +11,58 @@ module.exports = (robot) => {
     
     
     robot.respond(/操作$/i, (res) => {
-    res.send({
-      question: '操作',
-      options: ['借りる', '貸す', '一覧から選択']
+	res.send({
+	    question: '操作',
+	    options: ['借りる', '貸す', '一覧から選択']
+	});
     });
-  });
     
-  robot.respond('select', (res) => {
-    if (res.json.question === "操作") {
-      if (res.json.options[res.json.response] === "一覧から選択") {
-        res.send({
-          question: '選択',
-          options: ['完了', '編集', '支払い']
-        });
-      } else {
-	  //貸し借りまとめ 
-	  user=[];
-	  data=new Data();
-	  user.push(data);
-      }
-    } else if (res.json.question === "選択") {
-      if (res.json.options[res.json.response] === "完了") {
-        // 完了のコード
-      } else if (res.json.options[res.json.response] === "編集") {
-        // 編集のコード
-      } else if (res.json.options[res.json.response] === "支払い") {
-        // 支払いのコード
-      }
-    };
-  });
+    robot.respond('select', (res) => {
+	if (res.json.question === "操作") {
+	    if (res.json.options[res.json.response] === "一覧から選択") {
+		let user = robot.brain.get(res.message.user.name.toLowerCase());
+		let options = [];
+		let min=user.current_index + 5;
+		if(min>user.dataset.length) min=user.dataset.length;
+		for(let i=user.current_index; i<min; i+=1|0) {
+		    options.push(user.dataset[i]);
+		}
+		if(user.current_index+5<user.length){
+		    user.current_index += 5
+		    options.push("次の５件");
+		    res.send({
+			question: 'データ一覧',
+			options: options
+		    });
+		} else {
+		    res.send({
+			question: 'データ一覧',
+			options: options
+		    });
+		}
+		return;
+		
+		if(res.json.options[res.json.response] != "次の５件"){
+		    res.send({
+			question: '選択',
+			options: ['完了', '編集', '支払い']
+		    });
+		    if(res.json.options[res.json.response] === "完了"){
+			//完了のコード
+		    } else if(res.json.options[res.json.response] === "編集"){
+			//編集のコード
+		    } else if(res.json.options[res.json.response] === "支払い") {
+			//支払いのコード
+		    }
+		}
+		
+	    } else {
+		//貸し借りまとめ
+		user=[];
+		data=new Data();
+		user.push(data);
+	    }
+	}
+    });
 
 };
